e9df8e6c8167cb17c1baa8bdab818a0369ce3a96
Anuj12-2004 <anuj.harde_24uds@sanjivani.edu.in>
2024-12-30 16:18:59 +0530

    Create WelcomeMessage.java

---
+++ WelcomeMessage.java
@@ -0,0 +1,6 @@
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        // Print the welcome message to the console
+        System.out.println("Welcome to the CI/CD Pipeline Practical!");
+    }
+}
